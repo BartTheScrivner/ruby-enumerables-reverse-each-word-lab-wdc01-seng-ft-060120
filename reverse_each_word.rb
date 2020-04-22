395c760620d847337567e21a060c1da6fd96de27
fantastic-scope-1514 <dframe22@gmail.com>
2020-04-22 01:30:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   str_array = string.split
-  rev_array = str_array.map do |word|
+  rev_array = str_array.collect do |word|
     word.reverse
   end
   rev_array.join(" ")
